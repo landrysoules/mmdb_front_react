f2f87b9b11e40a0dfdba094eb7f726b10e249a12
Landry Soules <landry@spaceinvade.rs>
2018-07-12 22:43:07 +0200

    WIP people page

---
+++ src/actions/people.js
@@ -0,0 +1,24 @@
+import { PEOPLE_DETAILS } from '../constants/action-types';
+
+export function peopleDetails(id) {
+	return {
+		type: PEOPLE_DETAILS,
+		payload: {
+			request: {
+				url: `/people/${id}`,
+				params: {
+					api_key: process.env.REACT_APP_API_KEY
+				}
+			}
+		}
+	};
+}
+
+export function getPeopleDetails(id) {
+	return (dispatch, getState) => {
+		const details = getState().people[id];
+		if (!details) {
+			dispatch(peopleDetails(id));
+		}
+	};
+}
